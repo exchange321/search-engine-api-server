624f82181f3a0d365b477845376d4b071a954be5
Wayuki <adchuenhong@gmail.com>
2017-10-12 10:27:34 +1000

    Integrated common terms query into base query

--- src/middleware/search-document.js
+++ src/middleware/search-document.js
@@ -133,6 +133,8 @@ module.exports = function (options = {}) {
               query: q,
               type: 'best_fields',
               fields: ['title', 'body'],
+              cutoff_frequency: 0.001,
+              minimum_should_match: '3<75%',
             },
           },
           should: [
